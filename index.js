985764c3f231ec6c8580702c10cae50c8bd8333a
github username <github email address>
2017-09-28 13:34:49 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 	return Object.assign(driver, {[key]: value})
+	/*
+	driver[key] = value
+	return driver
+	 */
 }
 
 function deleteFromDriverByKey(driver, key) {
