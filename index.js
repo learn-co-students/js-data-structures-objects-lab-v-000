757c7d1f1bb5fb216bf2950384e52a9fb96c773a
veloblank <veloblank@gmail.com>
2017-11-04 08:38:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+let driver = {driver: "Sebastian Vettel"};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = Object.assign({}, driver, {[key]: value} );
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = delete[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
