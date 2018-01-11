3b93ad99ae830e8d518cfdbd54e6dafc82f20121
streamedLine <dovidlwtn@gmail.com>
2018-01-11 00:34:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+	return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+	driver[key] = value;
+	return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+	let obClone = Object.assign({}, driver);
+	delete obClone[key];
+	return obClone;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+	delete driver[key];
+	return driver;
+}
