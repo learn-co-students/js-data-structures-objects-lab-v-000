744b220f2f3afbfe6fb0cf06df29d8b2d3a90ce9
Robert Laws <roblaws@me.com>
2018-03-30 19:15:07 +0300

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue (driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
+}
+
+// function updateDriverWithKeyAndValue (driver, key, value) {
+//   const newObj = { ...driver }
+//   newObj[key] = value  
+//   return newObj
+// }
+
+// // // console.log(updateDriverWithKeyAndValue(driver, 'address', '11 Broadway'))
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value; 
+  return obj;
+}
+
+// function deleteFromDriverByKey(driver, key) {
+//   const newObj = { ...driver }
+//   delete newObj.key
+//   return newObj
+// }
+
+function deleteFromDriverByKey (driver, key) {
+  const newObj = Object.assign({}, driver);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
