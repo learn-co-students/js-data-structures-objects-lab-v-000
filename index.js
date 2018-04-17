5796336f327286f507e21f8fae519d651a0f108f
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-16 22:42:21 -0400

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const cloneObj = { ...driver };
-  const newObj = driver;
-  console.log(cloneObj);
-  console.log(newObj);
   delete cloneObj[key];
   return cloneObj;
 }
-// Why do I have to use the spread operator?
-// Why can't I do `cloneObject = driver;` ?
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
