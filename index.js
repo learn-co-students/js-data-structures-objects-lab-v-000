3fd452bafd300619e11fcff12a6beba18599c72d
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-23 19:18:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const driver = {
+  breakfast: 'Oatmeal',
+  lunch: 'Caesar salad',
+  dinner: 'Chimichangas'
+};
+
+
+function updateDriverWithKeyAndValue(obj, key, value){
+  const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+
+  return driver
+}
+
+function deleteFromDriverByKey(obj, key){
+  const newObj = { ...obj };
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+
+  return driver;
+}
