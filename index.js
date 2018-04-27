6027f1c4d46b1acf3c128c1a023c77719218a638
Olena Ageyeva <eageeva@hotmail.com>
2018-04-27 13:04:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const driver={
+  name: 'Sam'
+}
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  const newDriver={...driver};
+  return newDriver[key]=value
+}
