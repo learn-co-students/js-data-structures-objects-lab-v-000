c77e2eb3a7adb7fe42f2b456a8eb94d8f5484c18
jacobsilver2 <jacobsilver2@mac.com>
2018-05-01 08:07:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  let newDriver = {...driver};
-  return newDriver[key] = value;
+  return Object.assign({}, driver, {[key]: value} );
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let newDriver = {...driver};
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
+
+
+
