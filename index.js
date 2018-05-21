09d4f72df7e525f7af180d674ce171df5b01e60c
MyMac <mymac@JCB.local>
2018-05-21 15:45:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Write your solution in this file!
-const driver = {}
+const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
 	return Object.assign({}, driver, {[key]: value});
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-	return Object.assign(driver, {[key]: value});
+	driver[key] = value;
+	return driver;
 }
 
 function deleteFromDriverByKey(driver, key) {
@@ -16,5 +17,6 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-	return delete driver[key];
+	delete driver[key];
+	return driver;
 }
