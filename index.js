c69df4ee065ba4575b283da99dbf5a9296e07640
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-02 19:06:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const nu_driver = {... driver};
+    nu_driver[key] = value;
+    return nu_driver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const nu_driver = {...driver};
+    delete nu_driver[key]
+    return nu_driver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
