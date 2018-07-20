63917debdd6aa73b613066509a15e8bbe0f7df90
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-20 11:50:00 -0700

    two functions work and 5 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+}
+
