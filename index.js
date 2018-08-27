5a0cc0e0e5ec5d72e6b2c75e2d71f9e2c4183ed7
github username <github email address>
2018-08-27 12:22:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // Write your solution in this file!
+
+// defines a driver
 const driver = {}
 
+//returns a driver with the original key/value pair without modifying object driver(nondestructive)
 function updateDriverWithKeyAndValue(object, key, value){
   const newObject = {...object};
 	newObject[key] = value;
 	return newObject;
 }
 
+//returns a driver with the original key/value pair modifying object driver (destructive)
 function destructivelyUpdateDriverWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
 
+//delete(nondestructive)
 function deleteFromDriverByKey(object, key) {
   const newObject = {...object};
 	delete newObject[key];
 	return newObject;
 }
 
+//delete(destructive)
 function destructivelyDeleteFromDriverByKey(object, key) {
   delete object[key];
   return object;
