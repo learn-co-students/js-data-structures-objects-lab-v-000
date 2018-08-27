d89255dbaf17970779097f181ebff3106c347103
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-27 21:19:20 +0000

    Done.

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
 
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = { ...driver };
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
 }
