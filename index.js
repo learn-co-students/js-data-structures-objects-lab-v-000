2dd0c145062381b2e1c715327fbef5dd955180ca
Anusha Konatala <anu.konatala@gmail.com>
2018-09-02 04:14:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {name: "Sam"};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  
+  const newDriver = Object.assign({}, driver, { [key]: value });
+  return newDriver;
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key){
+  let newDriver = Object.assign({}, driver);
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
+}
