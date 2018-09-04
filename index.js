051dd5c6a9ad0f68b6d76832347102d8e43066ad
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 19:03:45 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value
   return driver
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const driver_two = Object.assign({}, driver)
+  delete driver_two[key]
+  return driver_two
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
