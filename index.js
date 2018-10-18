ee29b48a07172ee2baa906071016042e52983634
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-18 19:00:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+// defines a `driver` driver:
+const driver = {
+
+};
+
+
+// "before each" hook for "returns an driver with the original key value pairs and the new key value pair", does not modify the original driver, but rather returns a clone with the new data:
+function updateDriverWithKeyAndValue(driver, key, value) {
+const newDriver = {...driver};
+newDriver[key] = value;
+
+return newDriver;
+};
+
+
+// updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver:
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+driver[key] = value;
+
+return driver;
+};
+
+
+// deletes `key` from a clone of driver and returns the new driver (it is non-destructive), does not modify the original driver:
+function deleteFromDriverByKey(driver, key) {
+const newDriver = {...driver };
+delete newDriver[key];
+return newDriver;
+};
+
+
+// returns driver without the delete key/value pair, modifies the original driver:
+function destructivelyDeleteFromDriverByKey(driver, key) {
+delete driver[key];
+return driver;
+};
+
+
+
