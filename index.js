937b2003f5f21b7ed98e91d110c19bd81db9cf24
jonathan <jonathanyi81@gmail.com>
2018-11-01 00:55:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, value){
+  return Object.assign({}, obj, {[key]: value})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+
+//cannot use variable with dot notation
+//when using brackets have to use string or variable
+function deleteFromDriverByKey(obj, key){
+  const newObj = {...obj};
+  delete newObj[key]
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key){
+  delete obj[key]
+  return obj;
+}
