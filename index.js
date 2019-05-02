6afe9de688a8e95608465b9c16fd03d1bed726f2
sjwakean <swakeman@ca.rr.com>
2019-05-02 07:25:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var drivers = {
+    //name:’Sam’,
+    //address: '11 Broadway'
+};
+    
+
+//function updateDriverWithKeyAndValue() {
+    //return Object.assign({}, obj, { [key]: value});
+//}
+//const driver = {};
