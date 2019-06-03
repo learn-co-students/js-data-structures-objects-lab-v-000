9bd5d67c7e79943a61022eabcfcc16654a7fa795
Ashley <ashleywong678@gmail.com>
2019-06-03 11:55:47 -0400

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,12 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key){
-  delete driver.key;
+  const newDriver = Object.assign({}, driver)
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
 }
