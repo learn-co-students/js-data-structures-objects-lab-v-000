4a5c888f62ee20e507da3a6dddb1f0365a41e9b9
github username <github email address>
2019-07-09 16:09:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = { a_driver: "bob" };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = { ...driver };
+  newDriver[key] = value;
+
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    //const newDriver = { ...driver };
+    const newDriver = Object.assign({}, driver);
+    newDriver[key] = value;
+
+    delete newDriver['name'];
+     return newDriver;
+}
