497cc739f046979794f7d56c91abdaff6e20d91c
github username <github email address>
2019-10-03 13:28:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 // Write your solution in this file!
-const driver = {};
+// const driver = {};
+
+// function updateDriverWithKeyAndValue(driver, key, value) {
+//   return Object.assign({}, driver, {[key]: value});
+// }
+
+
+// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+//     driver[key] = value;
+//     return driver;
+// }
+
+// function deleteFromDriverByKey(driver, key){
+//   const newObject = Object.assign({}, driver);
+
+//   delete newObject[key];
+
+//   return newObject;
+
+//  }
+
+// function destructivelyDeleteFromDriverByKey(driver, key) {
+//   delete driver[key];
+//   return driver;
+// }
+
+const driver = {} 
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, driver, {[key]: value});
+	return Object.assign(driver, {|key|: value})
 }
-
-
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-    driver[key] = value;
-    return driver;
+	driver[key] = value;
+	return driver
 }
-
-function deleteFromDriverByKey(driver, key){
-  const newObject = Object.assign({}, driver);
-
-  delete newObject[key];
-
-  return newObject;
-
+ function deleteFromDriverByKey(driver, key){
+	const newDriver = Object.assign({}, driver)
+	delete newDriver[key]
+	return newDriver
  }
 
-function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key];
-  return driver;
-}
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+ 	delete driver.key
+ 	return driver
+ }
 
