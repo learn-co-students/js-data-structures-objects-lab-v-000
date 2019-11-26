b3e6c14f52d1e85e6f09ae387f248ee29f6508d5
ElvinGarcia <dimix181@gmail.com>
2019-11-25 23:37:15 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+    
+let driver = {};
+        function updateDriverWithKeyAndValue(obj, key, value) {
+          /* This function should not mutate the obj and
+            should return a new obj that has an updated value for the key passed in.
+       */
+          let newObj = {...obj};
+          newObj[key] = value;
+          return newObj;
+        }
+        
+        function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+            // same as above but destructive
+            obj[key] = value;
+            return obj;
+        }
+
+        function deleteFromDriverByKey(obj, key) {
+            // delete the key value pair for the key
+            // should not mutate the passed obj
+            let newObj = {...obj};
+             delete newObj[key];
+             return newObj;
+            
+        }
+
+        function destructivelyDeleteFromDriverByKey(obj, key) {
+            // same as above but destructive
+            delete obj[key];
+            return obj;
+        }
