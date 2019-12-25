b8941eac2ad728d6ae9c7ea100530930c81eba1b
Mohammed Noori <kmanoorib@gmail.com>
2019-12-25 04:27:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Write your solution in this file!
+// has solution already???
+
+// How to use Object.assign()
+// Object.assign(initialObject, additionalObject, additionalObject, ...);
+// Object.assign(initialObject, key, value);
+// Object.assign({}, initialObject); the initialObject already has key value pairs
+// Object.assign() is a good way to NOT destructively Update Object
+
 const driver = {};
 
-function updateDriverWithKeyAndValue(driver, key, value) {
-  //const newDriver = {...driver};
-  //newDriver.key = value;
-  return Object.assign({}, driver, {[key]: value});
-}
+// from lesson
+// function nondestructivelyUpdateObject(obj, key, value) {
+//   return Object.assign({}, obj, { [key]: value });
+// }
 
 //function nondestructivelyUpdateObject(obj, key, value) {
 //  const newObj = { ...obj };
@@ -15,10 +22,19 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 //  return newObj;
 //}
 
-//Object.assign(initialObject, additionalObject, additionalObject, ...);
+function updateDriverWithKeyAndValue(driver, key, value) {
+  // const newDriver = {...driver};
+  // newDriver.key = value;
+  // return newDriver; doesn't return new and original doesn't pass test
+  return Object.assign({}, driver, {[key]: value});
+}
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   // driver.key = value;
+  // driver["key"];
+  // const key = "something"
+  // driver[key]
+  
   driver[key] = value; //this way passes test unlike driver.key = value;
   return driver;
 }
@@ -26,6 +42,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   // const newDriver = {...driver};
   // above way works, but this one looks better
+  
   const newDriver = Object.assign({}, driver);
   delete newDriver[key];
   return newDriver;
