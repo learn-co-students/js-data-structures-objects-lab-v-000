ba6156cfc29e3b8307c13a69b1ae71382688c65f
sdcro <sdcrouse91@gmail.com>
2020-04-25 16:48:48 -0700

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const driver = {name: "Steven", favoriteFood: "Sausage omelette"};
 
 function updateDriverWithKeyAndValue (driver, key, value) {
-  return Object.assign({}, driver, { [key]: value });
+  // return Object.assign({}, driver, { [key]: value });
+  return { ...driver, [key]: value }; // Another way to do that
 }
 
 function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
@@ -13,6 +14,7 @@ function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
 
 function deleteFromDriverByKey (driver, key) {
   const driverCopy = Object.assign({}, driver); // Copy/clone the driver
+  // const driverCopy = { ...driver } // Another way to do that
   delete driverCopy[key]; // Delete the key/value pair from this driver copy
   return driverCopy; // Return that driver copy
 }
