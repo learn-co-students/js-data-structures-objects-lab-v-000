0580d0f80e0e7c390d2326e448c3c4a589eaaf88
github username <github email address>
2020-05-02 13:28:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Write your solution in this file!
 
-let driver = {}
-
-function updateDriverWithKeyAndValue(driver, key, value) {
-  let object2 = { ...driver };
-  object2[key] = value;
-  return object2;
-}
-
-
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
-  return driver;
-}
-
-function deleteFromDriverByKey(driver, key) {
-  let newDriver = { ...driver };
-  delete newDriver[key]
-  return newDriver
-}
-
-function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key]
-  return driver
-}
+// let driver = {}
+//
+// function updateDriverWithKeyAndValue(driver, key, value) {
+//   let object2 = { ...driver };
+//   object2[key] = value;
+//   return object2;
+// }
+//
+//
+// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+//   driver[key] = value;
+//   return driver;
+// }
+//
+// function deleteFromDriverByKey(driver, key) {
+//   let newDriver = { ...driver };
+//   delete newDriver[key]
+//   return newDriver
+// }
+//
+// function destructivelyDeleteFromDriverByKey(driver, key) {
+//   delete driver[key]
+//   return driver
+// }
