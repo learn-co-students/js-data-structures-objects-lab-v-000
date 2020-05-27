55ef83357e73807aa257e1e61930f6d34d628ccc
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-27 09:40:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    //clone object and add new record to cloned object
+    const newObj = Object.assign({}, driver)
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    //add to original object and update data
+    driver[key] = value;
+    return driver;
+};
+
+function deleteFromDriverByKey(driver, key) {
+    //clone object and delete data when given a key
+    const newObj = Object.assign({}, driver);
+    delete newObj[key];
+    return newObj;
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    //delete original object item when given a key
+    delete driver[key];
+    return driver;
+};
