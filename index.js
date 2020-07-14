2565636e824529326e41e222d03417af101f475a
dani8439 <dani8439@aol.com>
2020-07-14 13:25:52 -0400

    Done.

--- index.js
+++ index.js
@@ -2,27 +2,31 @@
 
 const driver = {};
 
+
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newObj = { ...driver };
+  const newObj = { ...driver};
 
   newObj[key] = value;
 
   return newObj;
+
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
 
   return driver;
+
 }
 
 function deleteFromDriverByKey(driver, key) {
-  const newObj = { ...driver };
+  const newObj = { ...driver};
   // const newObj = Object.assign({}, driver);
 
   delete newObj[key];
 
   return newObj;
+
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
@@ -31,3 +35,9 @@ function destructivelyDeleteFromDriverByKey(driver, key) {
   return driver;
 
 }
+
+
+
+
+
+//
