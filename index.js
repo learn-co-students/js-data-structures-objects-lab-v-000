5f09fffc6e8d7099c0ce98a91d41edafcbd501b4
Pat  Pignataro <pat6284@msn.com>
2020-09-03 12:34:11 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = {...driver};
   newDriver[key] = value;
   return newDriver;
+  
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
